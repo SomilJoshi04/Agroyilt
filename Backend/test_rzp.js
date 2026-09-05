500b9f565fae472eea797d1022ba71d5dc0c8806
Somil <somiljoshi7047@gmail.com>
2026-09-05 17:13:02 +0530

    chore: untrack test and diagnose scripts from Backend directory

--- Backend/test_rzp.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const { createOrder } = require('./services/razorpayService');
-
-async function test() {
-  const result = await createOrder(100, 'INR', 'receipt_test_1', { type: 'test' });
-  console.log(result);
-}
-
-test();
